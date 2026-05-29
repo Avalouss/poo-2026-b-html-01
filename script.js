c8af0fc94f88aa40614a7f832c97d24454fd2b2d
Carlos Avalos <20253ds046@utez.edu.mx>
2026-05-29 14:45:40 -0600

    [UPDATE] Practica completa

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ console.log(btnSaludar);
 
 btnSaludar.addEventListener("click", function(){
 
-    if(mensajeMostrado) {
+    console.log("click");
+
+    if(!mensajeMostrado) {
     const seccionContacto = document.getElementById("contacto");
 
    const mensajes = document.createElement("p");
@@ -15,7 +17,13 @@ btnSaludar.addEventListener("click", function(){
    mensajes.style.color = "#27ae60";
    mensajes.style.fontWeight = "bold";
 
+seccionContacto.appendChild(mensajes);
+mensajeMostrado = true;
+btnSaludar.textContent = "Mensaje Enviado";
+btnSaludar.disabled = true;
+btnSaludar.style.backgroundColor = "#bdc3c7";
+
    }
 
-   
+
 } )
